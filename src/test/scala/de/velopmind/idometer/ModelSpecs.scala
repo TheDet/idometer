a9729b37d1008c8f31af88d36a844b72c08a8e65
Dirk Detering <mailtodet@googlemail.com>
2011-05-23 16:29:05 +0200

    adjust formatting

--- src/test/scala/de/velopmind/idometer/ModelSpecs.scala
+++ src/test/scala/de/velopmind/idometer/ModelSpecs.scala
@@ -9,33 +9,33 @@ import java.util.Date
 
 @RunWith( classOf[JUnitRunner])
 class DurationSpec  extends FlatSpec with ShouldMatchers  {
-	"A Duration" should "represent a miliseconds span as hours (round down)" in {
-		val duration = Duration( (2 * 60 + 10) * 60 * 1000 )  // 2h 10m
-		duration.asHours should equal (2)
-	}
-	
-	it should "represent a miliseconds span as time in hh:mm format" in {
-		val shortDuration = Duration( (2 * 60 + 10) * 60 * 1000 )  // 2h 10m
-		shortDuration.asTime should equal ("2:10")
-		val longDuration = Duration( (26 * 60 + 10) * 60 * 1000 )  // 26h 10m
-		longDuration.asTime should equal ("26:10")
-	}
-	
-	it should "support addition" in {
-		val first  = Duration(300)
-		val second = Duration(900)
-		(first + second) should equal (Duration(1200))
-	}
+    "A Duration" should "represent a miliseconds span as hours (round down)" in {
+        val duration = Duration( (2 * 60 + 10) * 60 * 1000 )  // 2h 10m
+        duration.asHours should equal (2)
+    }
+    
+    it should "represent a miliseconds span as time in hh:mm format" in {
+        val shortDuration = Duration( (2 * 60 + 10) * 60 * 1000 )  // 2h 10m
+        shortDuration.asTime should equal ("2:10")
+        val longDuration = Duration( (26 * 60 + 10) * 60 * 1000 )  // 26h 10m
+        longDuration.asTime should equal ("26:10")
+    }
+    
+    it should "support addition" in {
+        val first  = Duration(300)
+        val second = Duration(900)
+        (first + second) should equal (Duration(1200))
+    }
 
 }
 
 @RunWith( classOf[JUnitRunner])
 class TimeSpec  extends FlatSpec with ShouldMatchers  {
-	import de.velopmind.idometer.Time._
-	"Time" should "represent long number as hours" in {
+    import de.velopmind.idometer.Time._
+    "Time" should "represent long number as hours" in {
         val time = 5.h
-		time should equal ( 5 * 60 * 60 * 1000)
-	}
+        time should equal ( 5 * 60 * 60 * 1000)
+    }
 }
 
 @RunWith( classOf[JUnitRunner])
@@ -47,7 +47,7 @@ class TimestampSpec  extends FlatSpec with ShouldMatchers  {
     }
     it should "return current date as long" in {
         val time = Timestamp.time
-	//time should equal ( 5 * 60 * 60 * 1000)
+    //time should equal ( 5 * 60 * 60 * 1000)
     }
     it should "use set factory" in {
       import java.text.SimpleDateFormat
@@ -71,13 +71,13 @@ class TimestampSpec  extends FlatSpec with ShouldMatchers  {
 @RunWith( classOf[JUnitRunner])
 class ActivitySpec  extends FlatSpec with ShouldMatchers  {
     "An Activity " should "start" in {
-	val activity = Activity("test", new Date(1000000L))
+    val activity = Activity("test", new Date(1000000L))
         activity.start should equal (new Date(1000000L))
         activity.stop  should be (None)
         activity.descr should be ("")
     }
     it should "start with default time 'now'" in {
-    	val activity = Activity("test")
+        val activity = Activity("test")
         activity.start should not be null
         activity.start.getClass() should be (classOf[java.util.Date])
         activity.stop  should be (None)
@@ -85,14 +85,14 @@ class ActivitySpec  extends FlatSpec with ShouldMatchers  {
     }
     it should "stop" in {
         val startDate = new Date()
-    	val activity = Activity("test", startDate).finish
+        val activity = Activity("test", startDate).finish
         activity.start should equal (startDate)
         activity.stop  should not be (None)
         activity.descr should be ("")
     }
     it should "take comment" in {
         val startDate = new Date()
-    	var activity = Activity("test", startDate).finish
+        var activity = Activity("test", startDate).finish
 
         activity.start should equal (startDate)
 
@@ -108,7 +108,7 @@ class ActivitySpec  extends FlatSpec with ShouldMatchers  {
     }
     it should "return duration" in {
         val startDate = new Date()
-    	val activity = Activity("test", startDate).finish
+        val activity = Activity("test", startDate).finish
 
         activity.start should equal (startDate)
 
@@ -126,7 +126,7 @@ class ActivitySpec  extends FlatSpec with ShouldMatchers  {
 class TaskSpec  extends FlatSpec with ShouldMatchers  {
     import de.velopmind.idometer.Time._
     "A Task" should "be initialised with default values" in {
-    	val task = Task("1", "first", Duration(5.h))
+        val task = Task("1", "first", Duration(5.h))
         task.id            should be ("1")
         task.descr         should be ("first")
         task.estimatedTime should equal (Duration(5.h))
@@ -135,10 +135,10 @@ class TaskSpec  extends FlatSpec with ShouldMatchers  {
     }
 
    "A Task" should "consume activity" in {
-    	val task     = Task("1", "first", Duration(5.h))
+        val task     = Task("1", "first", Duration(5.h))
         val activity = Activity("1", start = new Date(1000), stop = Some(new Date(130 * 1000)))
-    	val consumed = task.consume(activity) 
-    	consumed.consumedTime.asMinutes should equal (2) // 130 seconds rounded down
+        val consumed = task.consume(activity) 
+        consumed.consumedTime.asMinutes should equal (2) // 130 seconds rounded down
     }
 }
 
@@ -147,29 +147,29 @@ class RepositorySpec   extends FlatSpec with ShouldMatchers  {
     import de.velopmind.idometer.Time._
     "The Repository" should "accept Tasks" in {
         val (one, two, three) = (Task("1", "one",   Duration(1.h)),
-		                 Task("2", "two",   Duration(2.h)),
-		                 Task("3", "three", Duration(3.h)))
-	val repo = new Repository		                		 
-	repo.addTask( one )
-	repo.addTask( two )
-	repo.addTask( three )
-		
-	repo.allTasks.size should equal (3) 
+                         Task("2", "two",   Duration(2.h)),
+                         Task("3", "three", Duration(3.h)))
+    val repo = new Repository                                 
+    repo.addTask( one )
+    repo.addTask( two )
+    repo.addTask( three )
+        
+    repo.allTasks.size should equal (3) 
     }
     it should "make Task current" in {
-	val (one, two, three) = (Task("1", "one",   Duration(1.h)),
-		                 Task("2", "two",   Duration(2.h)),
-		                 Task("3", "three", Duration(3.h)))
-	val repo = new Repository		                		 
-	repo.addTask( one )
-	repo.addTask( two )
-	repo.addTask( three )
-		
-	repo.makeCurrent( two )
-	repo.currentTask.get should equal ( two ) 
+    val (one, two, three) = (Task("1", "one",   Duration(1.h)),
+                         Task("2", "two",   Duration(2.h)),
+                         Task("3", "three", Duration(3.h)))
+    val repo = new Repository                                 
+    repo.addTask( one )
+    repo.addTask( two )
+    repo.addTask( three )
+        
+    repo.makeCurrent( two )
+    repo.currentTask.get should equal ( two ) 
 
-	repo.makeCurrent( three )
-	repo.currentTask.get should equal ( three ) 
+    repo.makeCurrent( three )
+    repo.currentTask.get should equal ( three ) 
     }
 
     it should "start and stop current Task" in {
@@ -191,28 +191,28 @@ class RepositorySpec   extends FlatSpec with ShouldMatchers  {
         }
     
         implicit val testdate = "20110403"
-	val one = Task("1", "one", Duration(1.h))
-	val repo = new Repository		                		 
-	repo.addTask( one )
-	repo.makeCurrent( one )
+    val one = Task("1", "one", Duration(1.h))
+    val repo = new Repository                                 
+    repo.addTask( one )
+    repo.makeCurrent( one )
         mockdate("10:32:05")
-	repo.startCurrent()
-	repo.allActivities.size should equal (0)
+    repo.startCurrent()
+    repo.allActivities.size should equal (0)
         val act = repo.currentActivity
         act should not equal (None)
         checkactivity(act.get, "1", "", createdate("10:32:05"), None) //TODO : further verify act !
-	mockdate("10:33:35")
-	repo.stopCurrent("stopit")
+    mockdate("10:33:35")
+    repo.stopCurrent("stopit")
     
-	repo.allActivities.size should equal (1)
+    repo.allActivities.size should equal (1)
         checkactivity(repo.allActivities(0), "1", "stopit", createdate("10:32:05") , Some(createdate("10:33:35")))    
 
         mockdate("10:42:42")
-	repo.startCurrent()
-	mockdate("10:46:42")
-	repo.stopCurrent("stopagain")
+    repo.startCurrent()
+    mockdate("10:46:42")
+    repo.stopCurrent("stopagain")
 
-	repo.allActivities.size should equal (2)
+    repo.allActivities.size should equal (2)
         checkactivity(repo.allActivities(1), "1", "stopit", createdate("10:32:05") , Some(createdate("10:33:35")))    
         checkactivity(repo.allActivities(0), "1", "stopagain", createdate("10:42:42"), Some(createdate("10:46:42")))    
 
--- src/test/scala/de/velopmind/idometer/XmlPersistenceSpecs.scala
+++ src/test/scala/de/velopmind/idometer/XmlPersistenceSpecs.scala
@@ -11,20 +11,20 @@ import de.velopmind.idometer._
 
 @RunWith( classOf[JUnitRunner])
 class PersistenceSpec  extends FlatSpec with ShouldMatchers  {
-	import de.velopmind.idometer.Time._
+    import de.velopmind.idometer.Time._
     "A persistence" should "marshal and unmarshal an activity" in {
-         val act = Activity("1", new Date(), Some(new Date()), "actOne")
-         val persist = new Persistence()
-         val xml = persist.activityToXml(act)
-         val actagain = persist.xmlToActivity(xml)
+        val act = Activity("1", new Date(), Some(new Date()), "actOne")
+        val persist = new Persistence()
+        val xml = persist.activityToXml(act)
+        val actagain = persist.xmlToActivity(xml)
          
-         act should equal (actagain)   
+        act should equal (actagain)   
     }
     "A persistence" should "marshal and unmarshal a task" in {
         import de.velopmind.idometer.Time._
         val task = Task("one", "one's description",
-		                       Duration(2 h),  Duration(0), // will not be restored: Duration(1 h),  
-                           Nil, true ) 
+                        Duration(2 h),  Duration(0), // will not be restored: Duration(1 h),  
+                        Nil, true ) 
 //         val act = Activity("1", new Date(), Some(new Date()), "actOne")
          val persist = new Persistence()
          val xml = persist.taskToXml(task)
@@ -50,35 +50,35 @@ class PersistenceSpec  extends FlatSpec with ShouldMatchers  {
     }
 */
   "A persistence" should "store a repository as XML" in {
-          def mockdate(time:String )(implicit testdate:String) {
+        def mockdate(time:String )(implicit testdate:String) {
             Timestamp.datefactory = () => createdate(time)(testdate) 
-          }
+        }
 
-         def createdate(time:String )(implicit testdate:String) = {
+        def createdate(time:String )(implicit testdate:String) = {
             import java.text.SimpleDateFormat
             new SimpleDateFormat("yyyymmdd-hh:mm:ss").parse(testdate+"-"+time)
-         }
+        }
 
-         val (one, two, three) = (Task("1", "one",   Duration(1.h)),
-				                             Task("2", "two",   Duration(2.h)),
-				                             Task("3", "three", Duration(3.h)))
-          val repo = new Repository		                		 
-          repo.addTask( one )
-          repo.addTask( two )
-          repo.addTask( three )
+        val (one, two, three) = (Task("1", "one",   Duration(1.h)),
+                                 Task("2", "two",   Duration(2.h)),
+                                 Task("3", "three", Duration(3.h)))
+        val repo = new Repository
+        repo.addTask( one )
+        repo.addTask( two )
+        repo.addTask( three )
 
-          repo.makeCurrent( two )
+        repo.makeCurrent( two )
           
-          repo.allActivities = List(
+        repo.allActivities = List(
                                   Activity("1", new Date(), Some(new Date()), "actOne"),
                                   Activity("2", new Date(), Some(new Date()), "actTwo"),
                                   Activity("2", new Date(), Some(new Date()), "actThree"),
                                   Activity("2", new Date(), Some(new Date()), "actFour"),
                                   Activity("3", new Date(), Some(new Date()), "actFive"),
                                   Activity("3", new Date(), Some(new Date()), "actSix")
-                                  )
+                                 )
           
-          new Persistence().saveRepo("./Testidometer.xml", repo)
+        new Persistence().saveRepo("./Testidometer.xml", repo)
 
     }
 }
