90b643dc450ddc184688b7670b7e58e0d3491a06
Dirk Detering <mailtodet@googlemail.com>
2011-05-27 16:55:33 +0200

    first raw view (file opening and saving works)

--- src/main/scala/de/velopmind/idometer/View.scala
+++ src/main/scala/de/velopmind/idometer/View.scala
@@ -19,23 +19,63 @@ package de.velopmind.idometer
 import scala.swing._
 import Swing._
 
+import de.velopmind.idometer.xml._
+
 object IdometerGui  extends SimpleSwingApplication {
   var repo:Repository = new Repository()
+  var currentFile:Option[String] = None
+
+  val mainPanel = new BorderPanel {
+            import BorderPanel._
+      }  
   
-  val mainFrame = new Frame() {
-        preferredSize = (500,300)
+  val mainFrame = new MainFrame() {
+        preferredSize = (600,400)
         menuBar = new MenuBar {
                      contents += new Menu("File") {
-                       contents += new MenuItem( Action("Open") { println ("hello")} )  
-                       contents += new MenuItem( Action("Save") { println ("hello")} )  
+                       contents += new MenuItem( Action("Open")      { openFile() } )  
+                       contents += new MenuItem( Action("Save")      { saveFile() } )  
+                       contents += new MenuItem( Action("SaveAs...") { saveFileAs() } )  
+                       contents += new MenuItem( Action("Exit") { System.exit(0)} )  
+                     }
+                     contents += new Menu("Edit") {
+                       contents += new MenuItem( Action("Open")      { println ("hello")} )  
+                       contents += new MenuItem( Action("Save")      { println ("hello")} )  
                        contents += new MenuItem( Action("SaveAs...") { println ("hello")} )  
                      }
+                     contents += new Menu("View") {
+                       contents += new MenuItem( Action("Tasks")      { println ("hello")} )  
+                       contents += new MenuItem( Action("Activities")      { println ("hello")} )  
+                       contents += new MenuItem( Action("Stopwatch") { println ("hello")} )  
+                     }
                   }
-        val x = new Panel() {}
-        contents = new BorderPanel {
-            import BorderPanel._
-        }
+//        val x = new Panel() {}
+        contents = mainPanel
   }
   
   def top = mainFrame
+  
+    
+  def openFile() {
+    val fc = new FileChooser()
+    val res = fc.showOpenDialog(mainPanel)
+    if (res == FileChooser.Result.Approve) {
+        currentFile = Some(fc.selectedFile.getCanonicalPath)
+        repo = new Persistence().loadRepo(currentFile.get)
+        mainFrame.title = currentFile.get
+    }
+  }
+  
+  def saveFile() {
+    currentFile.foreach { f=> new Persistence().saveRepo( f , repo) }
+  }
+
+  def saveFileAs() {
+    val fc = new FileChooser()
+    val res = fc.showSaveDialog(mainPanel)
+    if (res == FileChooser.Result.Approve) {
+        new Persistence().saveRepo(fc.selectedFile.getCanonicalPath, repo)
+    }
+  }
+
 }
