59fea17ef764d1e6c61fcb41ee3877576e267624
Dirk Detering <mailtodet@googlemail.com>
2011-06-02 14:52:22 +0200

    enh. idometer.sh - extended classpath

--- idometer.sh
+++ idometer.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
-java -cp target/scala_2.9.0/idometer_2.9.0-0.1.jar:project/boot/scala-2.9.0/lib/scala-library.jar:lib_managed/scala_2.9.0/compile/scala-swing-2.9.0.jar de.velopmind.idometer.swing.IdometerGui
+LIB=lib_managed/scala_2.9.0/compile/
+
+BASEPATH=target/scala_2.9.0/idometer_2.9.0-0.1.jar:project/boot/scala-2.9.0/lib/scala-library.jar
+LIBPATH=${LIB}scala-swing-2.9.0.jar:${LIB}grizzled-slf4j_2.9.0-0.5.jar:${LIB}log4j-1.2.16.jar:${LIB}slf4j-api-1.6.1.jar:${LIB}slf4j-log4j12-1.6.1.jar
+
+java -cp $BASEPATH:$LIBPATH de.velopmind.idometer.swing.IdometerGui
